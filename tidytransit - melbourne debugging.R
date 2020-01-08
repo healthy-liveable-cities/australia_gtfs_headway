03cc196b7acfad7520a389b97fc7430e5b72212d
Carl Higgs <carl.higgs@rmit.edu.au>
2020-01-08 13:06:01 +1100

    example for extracting trams

--- tidytransit - melbourne debugging.R
+++ tidytransit - melbourne debugging.R
@@ -59,12 +59,51 @@ for (feed in gtfs_feeds) {
 
 # Then for interactive debugging, some example code bits to hone down on a particular tram stop on Sydney Rd
 # Checking out Albion St / Sydney Rd stop (trams missing in melbourne wtf)
-gtfs$stop_times%>%filter(stop_id==17320)%>%select(trip_id)
-gtfs$trips%>%filter(trip_id=='2680.T0.3-19-mjp-1.1.H')
-service_id ='3_T0+a5'
-daytime_freq%>%filter(service_id=='3_T0+a5')
-test <- get_stop_frequency(gtfs, start_hour = 7, end_hour = 19,service_ids=c(service_id),by_route = FALSE)
 
-
+# The following demonstrates that headway can be calculated for the #19 tram stops in the data
+service_ids = gtfs$stop_times%>%filter(stop_id==17320)%>%select(trip_id) %>% left_join(gtfs$trips,by='trip_id')%>%select(service_id)%>%unique()%>% pull(service_id)
+test <- get_stop_frequency(gtfs, start_hour = 7, end_hour = 19,service_ids=service_ids,by_route = FALSE)
+test%>%filter(stop_id==17320)
+# Note that despite dow ostensibly being weekdays by default, headway was nevertheless calculated for weekend routes
+gtfs$calendar%>% filter(service_id%in%(test%>%filter(stop_id==17320)%>%pull(service_id)))
 # Note that date filtering is not currently working
 # this is where I was at when we stepped out for meeting
+test2 <- get_stop_frequency(gtfs, start_hour = 7, end_hour = 19,service_ids=service_ids,dow=c(1,1,1,1,1,0,0),by_route = TRUE)
+test2%>%filter(stop_id==17320)
+# Note that despite dow ostensibly being weekdays by default, headway was nevertheless calculated for weekend routes
+gtfs$calendar%>% filter(service_id%in%(test2%>%filter(stop_id==17320)%>%pull(service_id)))
+
+# To restrict to valid days
+
+valid_days <- c('Monday','Tuesday','Wednesday','Thursday','Friday')
+valid_service_ids <- gtfs$.$date_service_table %>% 
+  mutate(dow = weekdays(date)) %>% 
+  filter(dow %in% valid_days) %>%
+  pull(service_id)
+
+# To restrict to modes on a valid day (example of tram)
+tram_services <- gtfs$trips%>%
+  left_join(gtfs$routes,by='route_id')%>%
+  filter(route_type==3)%>%
+  select(service_id)%>%
+  unique() %>% 
+  filter(service_id%in%valid_service_ids) %>%
+  pull(service_id)
+
+daytime_freq <- get_stop_frequency(gtfs, start_hour = 7, end_hour = 19,service_ids = tram_services,by_route = FALSE)
+# create spatial features for all stops
+stops_sf <- stops_as_sf(gtfs$stops)
+# identify frequent stops
+daytime_frequent_stops_sf <- stops_sf %>% right_join(daytime_freq, by="stop_id") 
+# find minimum service headway for each stop
+unique_stop_headway = aggregate(headway ~ stop_id, data = daytime_frequent_stops_sf, min,na.rm=TRUE) %>% 
+  left_join(stops_sf[,c('stop_id','geometry')], by="stop_id")
+frequent_stops <- unique_stop_headway %>% filter(headway <= 30)
+st_write(unique_stop_headway, dsn=gpkg_out, layer=paste0('all_',stub), layer_options = "OVERWRITE=YES" )
+st_write(frequent_stops, dsn=gpkg_out, layer=paste0('frequent_',stub), layer_options = "OVERWRITE=YES" )
+
+
+# Also note that the choice to select the minimum headway of the services is naive,
+# More appropriate is
+# 1. filter out weekend service IDs
+# 2. Multiply calendar matrix by 
